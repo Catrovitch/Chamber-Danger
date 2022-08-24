0ea4bca931baeb02e8bb199e2af57bf8e3520135
alexander <ale.engelhardt@outlook.com>
2022-08-24 17:25:50 +0300

    fixing rendering of dungeon

--- src/ui/draw_dungeon.py
+++ src/ui/draw_dungeon.py
@@ -8,34 +8,32 @@ def draw_dungeon(dungeon, map_height, map_width):
     height = map_height*10
     width = map_width*10
 
-    window = pygame.display.set_mode((width, height))
+    display = pygame.display.set_mode((width, height))
 
     wall_colour = (0, 0, 0)
-    chamber_colour = (92, 92, 92)
+    chamber_colour = (145, 88, 88)
 
-    clock = pygame.time.Clock()
+    square_x = 0
+    square_y = 0
+    
+    for y in range(dungeon.map_height):
+        for x in range(dungeon.map_width):
+            if int(dungeon.map[x][y]) == 1:
+                pygame.draw.rect(
+                    display, wall_colour, (square_x, square_y, square_x+10, square_y+10))
+                square_x += 10
+
+            else:
+                pygame.draw.rect(
+                    display, chamber_colour, (square_x, square_y, square_x+10, square_y+10))
+                square_x += 10
+
+        square_y += 10
+        square_x = 0
+
+    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-            square_x = 0
-            square_y = 0
-            for y in range(dungeon.map_height):
-                for x in range(dungeon.map_width):
-                    if dungeon.map[x][y] == 1:
-                        pygame.draw.rect(
-                            window, wall_colour, (square_x, square_y, square_x+10, square_y+10))
-                        square_x += 10
-
-                    else:
-                        pygame.draw.rect(
-                            window, chamber_colour, (square_x, square_y, square_x+10, square_y+10))
-                        square_x += 10
-
-                square_y += 10
-                square_x = 0
-
-        pygame.display.flip()
-
-        clock.tick(1)
